c04c9b5c067030b6610e54273a67c8c1739b6157
David-Mekhaiel <d_mekhaiel@msn.com>
2017-04-08 20:59:41 +0100

    Finished Toy App

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+	has_many :Microposts
+	validates FILL_IN, presence: true #Replace FILL_IN with the right code fuckerface.
+	validates FILL_IN, presence: true #Replace FILL_IN with the right code fuckerface.
 end
